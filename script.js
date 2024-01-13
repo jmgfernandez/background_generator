72c31c924758008e36ea2f4309f1ebe65ce3c566
Migz <juanmiguelganfernandez@gmail.com>
2024-01-13 21:59:31 +0800

    update displayed background value to hex instead of rgb center elements vertically increase size of input and button elements and add simple animations to them

--- script.js
+++ script.js
@@ -5,25 +5,27 @@ var body = document.getElementById("gradient");
 var random = document.querySelector("button");
 
 //to make code DRY
+//stored background value to var hexValue to display hex values instead of rgb
 function setGradient() {
-	body.style.background = "linear-gradient(to right, "+color1.value+", "+color2.value+")";
-	css.textContent = body.style.background+";";
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+  var hexValue =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+  css.textContent = hexValue + ";";
 }
 
 function startingGradient() {
-	color1.value = "#ff0000";
-	color2.value = "#ffff00";
-	body.style.background = "linear-gradient(to right, "+color1.value+", "+color2.value+")";
-	css.textContent = body.style.background+";";
+  color1.value = "#ff0000";
+  color2.value = "#ffff00";
+  setGradient();
 }
 
 function generateRandomColor() {
-	var randomColor1 = Math.floor(Math.random()*16777215).toString(16);
-	var randomColor2 = Math.floor(Math.random()*16777215).toString(16);
-	color1.value = "#"+randomColor1;
-	color2.value = "#"+randomColor2;
-  	body.style.background = "linear-gradient(to right, "+color1.value+", "+color2.value+")";
-	css.textContent = body.style.background+";";
+  var randomColor1 = Math.floor(Math.random() * 16777215).toString(16);
+  var randomColor2 = Math.floor(Math.random() * 16777215).toString(16);
+  color1.value = "#" + randomColor1;
+  color2.value = "#" + randomColor2;
+  setGradient();
 }
 
 startingGradient();
@@ -33,4 +35,3 @@ color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
 
 random.addEventListener("click", generateRandomColor);
-
